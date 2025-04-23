de9754f5c2df65766073af46236cdff3b0cc6608
Rex77pro <smaurieka@gmail.com>
2025-04-23 13:42:51 +0200

    Feature(AWS): WIP AWS setup

---
+++ DLS_Mandatory_Project/FileTransferService/Services/IS3Service.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FileTransferService.Services
+{
+    public interface IS3Service
+    {
+        Task<string> UploadFileAsync(IFormFile file);
+        Task<Stream> GetFileAsync(string key);
+    }
+}
---
+++ DLS_Mandatory_Project/FileTransferService/Services/S3Service.cs
@@ -0,0 +1,32 @@
+﻿
+using Amazon.S3;
+
+namespace FileTransferService.Services
+{
+    public class S3Service : IS3Service
+    {
+        private readonly IAmazonS3 _s3Client;
+        private readonly string _bucketName;
+
+        public S3Service(IConfiguration config)
+        {
+            var aws = config.GetSection("AWS");
+            _bucketName = aws["BucketName"];
+
+            _s3Client = new AmazonS3Client(
+                aws["AccessKey"],
+                aws["SecretKey"],
+                Amazon.RegionEndpoint.GetBySystemName(aws["Region"]));
+        }
+
+        Task<string> IS3Service.UploadFileAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Stream> IS3Service.GetFileAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
