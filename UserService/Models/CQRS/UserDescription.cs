6263393358ca96e4d14b97eb196843b24d330de3
Shero Shvan <sheroshvan@outlook.dk>
2025-04-29 08:44:55 +0200

    Create user class domain and DTO

---
+++ UserService/Models/CQRS/UserDescription.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
+using UserService.Interface;
+
+namespace UserService.Models.CQRS
+{
+    public class UserDescription : UserModel
+    {
+        public UserDescription()
+        {
+
+        }
+      
+
+        #region Navigation Properties
+        //public User User { get; set; }
+
+        #region represent the alternate key
+        public DateTime ModifiedDate { get; set; }
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        #endregion
+
+        #endregion
+
+    }
+}
+
---
+++ UserService/Models/DTO/UserInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserService.Models.DTO
+{
+    public class UserInfo : UserModel
+    {
+        public Guid Guid { get; set; }
+        public long LastModifiedTicks { get; set; }
+        public UserRole Role { get; set; }
+
+
+        public UserInfo()
+        {
+
+        }
+    }
+}
+public enum UserRole
+{
+    User
+}
