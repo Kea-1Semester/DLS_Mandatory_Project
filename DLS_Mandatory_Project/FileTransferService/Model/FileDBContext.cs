831362286481891b1a1c3dfc1bd189c2bef81fef
Rex77pro <smaurieka@gmail.com>
2025-05-26 14:32:12 +0200

    Feature(DB): Setup 1.1

--- DLS_Mandatory_Project/FileTransferService/Model/FileDBContext.cs
+++ DLS_Mandatory_Project/FileTransferService/Model/FileDBContext.cs
@@ -4,6 +4,11 @@ namespace FileTransferService.Model
 {
     public class FileDBContext : DbContext
     {
+        public FileDBContext(DbContextOptions options) 
+            : base(options) { }
+        
+        public DbSet<FileMetaDataModel> Files { get; set; }
 
+        
     }
 }
