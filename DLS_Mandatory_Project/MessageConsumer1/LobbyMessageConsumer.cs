c66489f5ab76890707231167d63d4ce8c7ef314f
Brezzor <oliver.bresson@gmail.com>
2025-06-09 10:02:52 +0200

    Seneste ændringer til rabbitmq consumer

--- DLS_Mandatory_Project/MessageConsumer1/LobbyMessageConsumer.cs
+++ DLS_Mandatory_Project/MessageConsumer1/LobbyMessageConsumer.cs
@@ -4,19 +4,19 @@ using System.Text.Json;
 
 public class LobbyMessageConsumer : IConsumer<LobbyMessage>
 {
-    private readonly MessageDbContext _dbContext;
+    //private readonly MessageDbContext _dbContext;
 
-    public LobbyMessageConsumer(MessageDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
+    //public LobbyMessageConsumer(MessageDbContext dbContext)
+    //{
+    //    _dbContext = dbContext;
+    //}
     public async Task Consume(ConsumeContext<LobbyMessage> context)
     {
         Console.WriteLine("Message received.");
         var jsonMessage = JsonSerializer.Serialize(context.Message);
         Console.WriteLine($"Received message: {jsonMessage}");
 
-        await _dbContext.AddAsync(context.Message);
-        await _dbContext.SaveChangesAsync();
+        //await _dbContext.AddAsync(context.Message);
+        //await _dbContext.SaveChangesAsync();
     }
 }
---
+++ DLS_Mandatory_Project/MessageConsumer1/Program.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+
+var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+{
+    cfg.Host("rabbitmq", "/", h =>
+    {
+        h.Username("guest");
+        h.Password("guest");
+    });
+    cfg.ReceiveEndpoint("lobby-message-queue", e =>
+    {
+        e.Consumer<LobbyMessageConsumer>();
+    });
+});
+
+Console.WriteLine("Starting consumer...");
+bus.Start();
+
+try
+{
+    Console.WriteLine("Receiving messages. Press a key to stop...");
+    Console.Read();
+}
+finally
+{
+    Console.WriteLine("Stopping consumer...");
+    bus.Stop();
+}
